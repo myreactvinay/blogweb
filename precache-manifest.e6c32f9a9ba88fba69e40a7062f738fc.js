a1b2a6c4aa866c8cd90067e664a03585f30ee4a4
Vinay <tanakala.vinay@gmail.com>
2020-05-04 22:58:54 +0530

    Updates

---
+++ precache-manifest.e6c32f9a9ba88fba69e40a7062f738fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96b8e66cd11150cd5023e24d653479b",
+    "url": "/blogweb/index.html"
+  },
+  {
+    "revision": "12cc52f3619fa4a80c73",
+    "url": "/blogweb/static/css/2.debc511a.chunk.css"
+  },
+  {
+    "revision": "eb4d175f30bb29bee6ab",
+    "url": "/blogweb/static/css/main.73cd077b.chunk.css"
+  },
+  {
+    "revision": "12cc52f3619fa4a80c73",
+    "url": "/blogweb/static/js/2.d420ab7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blogweb/static/js/2.d420ab7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb4d175f30bb29bee6ab",
+    "url": "/blogweb/static/js/main.ffc84534.chunk.js"
+  },
+  {
+    "revision": "2e7b98582887a9de42b7",
+    "url": "/blogweb/static/js/runtime-main.a78dbf25.js"
+  }
+]);
